07cdc547a73efa55570bcc86cc6ed9e990dbf9ae
BabayevAsad <esed.babayev2002@gmail.com>
2026-06-19 19:18:38 +0200

    feat:implement longest substring without repeating characters using sliding windows

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ import org.example.slidingWindow.SlidingWindow;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(SlidingWindow.maxSumSubArray(new int[]{2,1,5,1,3,2},3));
+        System.out.println(SlidingWindow.longestSubstringWithoutRepeating("bacaefgh"));
     }
 }
--- src/main/java/org/example/slidingWindow/SlidingWindow.java
+++ src/main/java/org/example/slidingWindow/SlidingWindow.java
@@ -1,5 +1,8 @@
 package org.example.slidingWindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SlidingWindow {
 
     /// Example inputs:{2,1,5,1,3,2} k=3; {1,12,-5,-6,50,3} k=3
@@ -21,5 +24,29 @@ public class SlidingWindow {
         }
 
         return maxSum;
+    }
+
+    /// example inputs:"bacaefgh", "pvavkepv";
+    /// Longest Substring Without Repeating Characters
+    public static int longestSubstringWithoutRepeating(String text){
+        if (text==null || text.length()==0) return 0;
+        if (text.length()==1) return 1;
+
+        Map<Character,Integer> map= new HashMap<>();
+        int left=0;
+        int max=0;
+
+        for(int i=0;i<text.length();i++){
+            char currentChar = text.charAt(i);
+
+            if(map.containsKey(currentChar)){
+                left=Math.max(left,map.get(currentChar)+1);
+            }
+
+            map.put(currentChar,i);
+            max=Math.max(max,i-left+1);
+        }
+
+        return max;
     }
 }
