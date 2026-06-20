d0dc6d93ff968fa4016a1eb1cfb87952a2fa707f
BabayevAsad <esed.babayev2002@gmail.com>
2026-06-20 21:12:43 +0200

    feat:implement ticket queue simulation with array boundaries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
 
-import org.example.stackandqueue.Stack;
+import org.example.stackandqueue.Queue;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Stack.baseballGame(new String []{"5", "2", "C", "D", "+", "9", "+", "+"}));
+        System.out.println(Queue.buyingTicket(new int[]{1, 1, 5, 1, 1},2));
     }
 }
--- src/main/java/org/example/stackandqueue/Queue.java
+++ src/main/java/org/example/stackandqueue/Queue.java
@@ -1,4 +1,36 @@
 package org.example.stackandqueue;
 
+import java.util.ArrayDeque;
+
 public class Queue {
+
+    /// example inputs tickets: {2, 3, 2}, customerIndex:2; tickets = {1, 1, 5, 1, 1}, customerIndex = 2
+    /// Time Needed to Buy Tickets
+    public static int buyingTicket(int [] tickets, int customerIndex){
+        if (customerIndex < 0 || customerIndex >= tickets.length) return -1;
+
+        java.util.Queue<int[]> queue = new ArrayDeque<>();
+
+        for(int i = 0; i < tickets.length; i++){
+            queue.offer(new int[]{tickets[i], i});
+        }
+
+        int time=0;
+
+        while(!queue.isEmpty()){
+            int[] customer =  queue.poll();
+
+            customer[0]--;
+            time++;
+
+            if(customer[1]==customerIndex && customer[0]==0){
+                return time;
+            }
+
+            if(customer[0]>0){
+                queue.offer(customer);
+            }
+        }
+        return time;
+    }
 }
