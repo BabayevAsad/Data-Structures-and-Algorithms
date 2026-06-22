378b610c12f37f57a38fc78b8af8950f4b6806fd
BabayevAsad <esed.babayev2002@gmail.com>
2026-06-22 23:49:07 +0200

    feat:implement find peak element using binary search

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ import org.example.prexifsum.PrefixSum;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(BinarySearch.searchInsertPosition(new int[]{1,2,3,4,5,6,7,9,10},8));
+        System.out.println(BinarySearch.findPeakElement(new int[]{1, 2, 3, 1}));
     }
 }
--- src/main/java/org/example/binarysearch/BinarySearch.java
+++ src/main/java/org/example/binarysearch/BinarySearch.java
@@ -48,4 +48,23 @@ public class BinarySearch {
         }
         return left;
     }
+
+    /// example inputs: {1, 2, 1, 3, 5, 6, 4}, {1, 2, 3, 1}
+    /// Return its index
+    public static int findPeakElement(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
 }
