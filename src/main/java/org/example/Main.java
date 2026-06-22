025955ef949766afeb528e5f419011e9ab2f6b17
BabayevAsad <esed.babayev2002@gmail.com>
2026-06-22 23:38:45 +0200

    feat:implement search insert position with binary search

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ import org.example.prexifsum.PrefixSum;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(BinarySearch.binarySearch(new int[]{1,2,3,4,5,6,7,8,9,10},2));
+        System.out.println(BinarySearch.searchInsertPosition(new int[]{1,2,3,4,5,6,7,9,10},8));
     }
 }
--- src/main/java/org/example/binarysearch/BinarySearch.java
+++ src/main/java/org/example/binarysearch/BinarySearch.java
@@ -24,4 +24,28 @@ public class BinarySearch {
 
         return -1;
     }
+
+    /// example inputs:{1,2,3,4,5,6,7,9,10} target=8; {3, 5, 7, 9} target:1
+    public static int searchInsertPosition(int[] arr, int target){
+        if(arr == null || arr.length == 0) return -1;
+        int left=0;
+        int right=arr.length-1;
+
+
+        while(left<=right){
+            int mid = left + (right-left)/2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+
+            if(target>arr[mid]){
+                left=mid+1;
+            }
+            else if(target<arr[mid]){
+                right=mid-1;
+            }
+        }
+        return left;
+    }
 }
